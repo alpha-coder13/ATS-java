c4edc56d1b62e73045d5c5a9fe5041ab437a9e48
alpha-coder13 <amardeepsaha13@gmail.com>
2024-11-01 18:08:47 +0530

    java atm application - first commit

--- bms/src/bms/Login.java
+++ bms/src/bms/Login.java
@@ -2,8 +2,14 @@ package src.bms;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener; //interafce class to listen action performed
 
-public class Login  extends JFrame{
+public class Login  extends JFrame implements ActionListener {
+
+    JButton loginButton, clearButton, signUpButton;
+    JTextField cardText;
+    JPasswordField pinText;
     Login(){
         setTitle("Automated Teller machine"); // window header
         setLayout(null);//sets the layout of the document
@@ -23,7 +29,7 @@ public class Login  extends JFrame{
         cardNumber.setFont(new Font("Arial", Font.BOLD, 16 ));
         add(cardNumber);
 
-        JTextField cardText =  new JTextField();
+        cardText =  new JTextField();
         cardText.setBounds(400, 120, 300 , 20);
         add(cardText);
 
@@ -34,28 +40,49 @@ public class Login  extends JFrame{
         pin.setFont(new Font("Arial", Font.BOLD, 16 ));
         add(pin);
 
-        JTextField pinText =  new JTextField();
+        pinText =  new JPasswordField();
         pinText.setBounds(400, 180, 300 , 20);
         add(pinText);
 
 
         // button for login form
-        JButton loginButton = new JButton("Click to Login");
+        loginButton = new JButton("Click to Login");
         loginButton.setBounds(400, 240, 150 , 40);
         loginButton.setBackground(Color.BLACK);
         loginButton.setForeground(Color.white);
+        loginButton.addActionListener(this);
         add(loginButton);
 
-        JButton clearButton = new JButton("clear");
+        clearButton = new JButton("clear");
         clearButton.setBounds(575, 240, 100 , 40);
         clearButton.setBackground(Color.BLACK);
         clearButton.setForeground(Color.white);
+        clearButton.addActionListener(this);
         add(clearButton);
+
+        signUpButton = new JButton("clear");
+        signUpButton.setBounds(575, 240, 100 , 40);
+        signUpButton.setBackground(Color.BLACK);
+        signUpButton.setForeground(Color.white);
+        signUpButton.addActionListener(this);
+        add(signUpButton);
 
         setSize(800 , 400); // sets the size of the frame
         setVisible(true); // sets the visibility of the frame // default is false;
         setResizable(false);
         setLocation(350 , 200);// sets the distance from the origin of the screen
+    }
+    public void actionPerformed(ActionEvent ae){ // overriding the method of the interface actionlisterner
+        System.out.println(ae.getSource() == loginButton);  // ae is the ActionEvent class to get the event details
+        // sample out put :  java.awt.event.ActionEvent[ACTION_PERFORMED,cmd=Click to Login,when=1730461760204,modifiers=Button1] on javax.swing.JButton[,400,240,150x40,alignmentX=0.0,alignmentY=0.5,border=javax.swing.plaf.BorderUIResource$CompoundBorderUIResource@31d294de,flags=296,maximumSize=,minimumSize=,preferredSize=,defaultIcon=,disabledIcon=,disabledSelectedIcon=,margin=javax.swing.plaf.InsetsUIResource[top=2,left=14,bottom=2,right=14],paintBorder=true,paintFocus=true,pressedIcon=,rolloverEnabled=true,rolloverIcon=,rolloverSelectedIcon=,selectedIcon=,text=Click to Login,defaultCapable=true]
+        if(ae.getSource() == loginButton){
+            // call login pages
+        }else if(ae.getSource() == clearButton){
+            cardText.setText("");
+            pinText.setText("");
+        }else if(ae.getSource() == signUpButton){
+            // calls signup class
+        }
     }
     public static void main(String[] args){
         new Login();
